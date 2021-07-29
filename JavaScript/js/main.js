8eb0cbcfa27ed3e4774636bca68dc1033ff8dd4b
EvandroMiguel <evandro@azoup.com.br>
2021-07-29 13:30:03 -0300

    adicionado javascript

---
+++ JavaScript/js/main.js
@@ -0,0 +1,15 @@
+var nome = "Evandro Luiz Miguel";
+var idade = 36;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+//alert("Meu Primeiro js"); 
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome + " tem " + idade + " anos");
+console.log(idade + idade2);
+console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+alert(frase.replace("Japão","Brasil"));
