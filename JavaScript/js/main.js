8ba9b065f8b59aedfa6960bc8e67c9845cdea236
EvandroMiguel <evandro@azoup.com.br>
2021-08-03 08:30:59 -0300

    lista e dicionario

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,3 +1,56 @@
+/*
+// Lista de dicionários
+var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
+
+console.log(frutas);
+
+console.log(frutas[1].nome);
+
+console.log(frutas[1].cor);
+
+
+// Dicionário
+var fruta = {nome:"maça", cor:"vermelha"};
+
+console.log(fruta);
+
+console.log(fruta.nome);
+
+console.log(fruta.cor);
+
+
+/*
+// lista
+var lista = ["maça", "pera", "laranja"];
+
+// exibe a lista
+console.log(lista);
+
+// adiciona item a lista
+lista.push("uva");
+
+// Exclui item da lista
+lista.pop();
+
+// Exibe o tamanho da lista
+console.log(lista.length);
+
+// inverte a ordem da lista
+console.log(lista.reverse());
+
+// exibe a lista como string
+console.log(lista.toString());
+
+// exibe o primeiro item da lista
+console.log(lista[0]);
+
+// exibe o primeiro caracter do primeiro item da lista
+console.log(lista.toString()[0]);
+
+// Adiciona uma informação na lista
+console.log(lista.join(" - "));
+
+/*
 var nome = "Evandro Luiz Miguel";
 var idade = 36;
 var idade2 = 10;
@@ -13,3 +66,4 @@ console.log(frase.toUpperCase());
 console.log(frase.toLowerCase());
 
 alert(frase.replace("Japão","Brasil"));
+*/
