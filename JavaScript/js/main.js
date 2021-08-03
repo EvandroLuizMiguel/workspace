e1a584ce7b09fb561e94b68c83763931ca2b98c7
EvandroMiguel <evandro@azoup.com.br>
2021-08-03 14:01:56 -0300

    funçoes javascript

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,4 +1,69 @@
 /*
+//var valida = 0;
+function validaIdade(idade){
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false 
+    }
+    return validar;
+}
+*/
+function validaIdade(idade){
+    if (idade >= 18){
+        return true
+    }else{
+        return false 
+    }
+}
+
+var idade = prompt("qual sua idade");
+console.log(validaIdade(idade));
+
+/*
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+function setReplace(frase,nome,novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+
+
+alert(soma(5,10));
+alert(setReplace("Vai Japão","Japão","Brasil"));
+
+/*
+var d = new Date();
+alert(d.getDay()+1);
+alert(d.getMonth()+1);
+alert(d.getFullYear());
+
+/*
+var count;
+for (count = 0; count <= 5; count++){
+    alert(count);
+}
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    //count = count + 1;  
+    count++;
+};
+
+/*
+// Pergunta para saber idade... abre opção para digitar
+var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+
+/*
 // Lista de dicionários
 var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
 
