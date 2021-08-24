eae740b0db295313d33fadbcaa17fb7983603ef4
EvandroMiguel <evandro@azoup.com.br>
2021-08-24 14:47:33 -0300

    adicionado pagina de informações

---
+++ Desafio_Netflix/js/funcoes.js
@@ -0,0 +1,7 @@
+function assistir() {
+  window.open('https://www.youtube.com/watch?v=mUQnfgvj5aA&ab_channel=StreamingsBrasil','_self');
+}
+
+function informacoes() {
+   window.open('informacoes.html','informacoes',"scrollbars=no");
+ }
