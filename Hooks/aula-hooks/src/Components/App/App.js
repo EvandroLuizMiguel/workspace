edcf03e9e3ee5b22662f66a0faa747573eb30625
EvandroMiguel <evandro@azoup.com.br>
2021-09-23 13:56:45 -0300

    pasta hooks

---
+++ Hooks/aula-hooks/src/Components/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Counter from '../Counter/Counter';
+import SmartCounter from '../SmartCounter/SmartCounter';
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello Word</h1>
+      <Counter/>
+      <SmartCounter/>
+    </>
+  );
+}
+
+export default App;
