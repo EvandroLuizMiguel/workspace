470b4793fcc409e35b4ca2b60e494fae90e7f498
EvandroMiguel <evandro@azoup.com.br>
2021-09-24 14:08:39 -0300

    usando hooks state

--- Hooks/aula-hooks/src/Components/App/App.js
+++ Hooks/aula-hooks/src/Components/App/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Counter from '../Counter/Counter';
 import SmartCounter from '../SmartCounter/SmartCounter';
-
+import Ifoodcounter from '../ifoodcounter/Ifoodcounter';
 
 function App() {
   return (
     <>
       <h1>Hello Word</h1>
       <Counter/>
-      <SmartCounter/>
+      <SmartCounter/>      
+      <Ifoodcounter/>
     </>
   );
 }
